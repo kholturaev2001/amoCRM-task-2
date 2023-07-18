7d09146b75448b256b77fac2b375f3d5aed80686
Muhammadrasul <holturaevm@gmail.com>
2023-07-18 16:54:26 +0300

    download font-family, add Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,30 @@
 import styles from "./Navbar.module.scss";
-import logo from '../../assets/icons/logo_welbex.svg'
-
+import logo from "../../assets/icons/logo_welbex.svg";
+import tg from '../../assets/icons/telegram_icon.svg'
+import wapp from '../../assets/icons/whatsapp.svg'
+import viber from '../../assets/icons/viber.svg'
 const Navbar = () => {
   return (
-    <nav>
-      <div className={styles.logo}>
-        <img src={logo} alt="logo" />
-      </div>
+    <nav className={styles.container}>
+      <section>
+        <div className={styles.logo}>
+          <img src={logo} alt="logo" />
+          <p>крупный интегратор CRM в Росcии и ещё 8 странах</p>
+        </div>
+        <ul>
+          <li>Услуги</li>
+          <li>Виджеты</li>
+          <li>Интеграции</li>
+          <li>Кейсы</li>
+          <li>Сертификаты</li>
+        </ul>
+      </section>
+      <section>
+        <span>+7 555 555-55-55</span>
+        <img src={tg} alt="logo" />
+        <img src={viber} alt="logo" />
+        <img src={wapp} alt="logo" />
+      </section>
     </nav>
   );
 };
