a54a4d9f3cfffab2f8c85ae628f079dce0f2935a
Muhammadrasul <holturaevm@gmail.com>
2023-07-18 22:26:46 +0300

    fix bug of footer 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="heart_content">
       <Background />
       <div className="app_content">
         <Navbar />
